aebf466dc76954b00d4f8c90672af8a6b9126dbe
Shetty <sachshet@visa.com>
2023-06-07 15:33:55 +0530

    pin debit samples

---
+++ samples/Payments/Payments/pin-debit-purchase-using-emv-technology-with-contactless-read-with-visa-platform-connect.py
@@ -0,0 +1,97 @@
+from CyberSource import *
+from pathlib import Path
+import os
+import json
+from importlib.machinery import SourceFileLoader
+
+config_file = os.path.join(os.getcwd(), "data", "Configuration.py")
+configuration = SourceFileLoader("module.name", config_file).load_module()
+
+# To delete None values in Input Request Json body
+def del_none(d):
+    for key, value in list(d.items()):
+        if value is None:
+            del d[key]
+        elif isinstance(value, dict):
+            del_none(value)
+    return d
+
+def pin_debit_purchase_using_emv_technology_with_contactless_read_with_visa_platform_connect():
+    clientReferenceInformationCode = "2.2 Purchase"
+    clientReferenceInformation = Ptsv2paymentsClientReferenceInformation(
+        code = clientReferenceInformationCode
+    )
+
+    processingInformationCapture = False
+    processingInformationCommerceIndicator = "retail"
+    processingInformation = Ptsv2paymentsProcessingInformation(
+        capture = processingInformationCapture,
+        commerce_indicator = processingInformationCommerceIndicator
+    )
+
+    paymentInformation = Ptsv2paymentsPaymentInformation(
+    )
+
+    orderInformationAmountDetailsTotalAmount = "202.00"
+    orderInformationAmountDetailsCurrency = "USD"
+    orderInformationAmountDetails = Ptsv2paymentsOrderInformationAmountDetails(
+        total_amount = orderInformationAmountDetailsTotalAmount,
+        currency = orderInformationAmountDetailsCurrency
+    )
+
+    orderInformation = Ptsv2paymentsOrderInformation(
+        amount_details = orderInformationAmountDetails.__dict__
+    )
+
+    pointOfSaleInformationEntryMode = "contactless"
+    pointOfSaleInformationTerminalCapability = 4
+    pointOfSaleInformationEmvTags = "9F3303204000950500000000009F3704518823719F100706011103A000009F26081E1756ED0E2134E29F36020015820200009C01009F1A0208409A030006219F02060000000020005F2A0208409F0306000000000000"
+    pointOfSaleInformationEmvCardSequenceNumber = "1"
+    pointOfSaleInformationEmvFallback = False
+    pointOfSaleInformationEmv = Ptsv2paymentsPointOfSaleInformationEmv(
+        tags = pointOfSaleInformationEmvTags,
+        card_sequence_number = pointOfSaleInformationEmvCardSequenceNumber,
+        fallback = pointOfSaleInformationEmvFallback
+    )
+
+    pointOfSaleInformationTrackData = "%B4111111111111111^JONES/JONES ^3112101976110000868000000?;4111111111111111=16121019761186800000?"
+    pointOfSaleInformation = Ptsv2paymentsPointOfSaleInformation(
+        entry_mode = pointOfSaleInformationEntryMode,
+        terminal_capability = pointOfSaleInformationTerminalCapability,
+        emv = pointOfSaleInformationEmv.__dict__,
+        track_data = pointOfSaleInformationTrackData
+    )
+
+    requestObj = CreatePaymentRequest(
+        client_reference_information = clientReferenceInformation.__dict__,
+        processing_information = processingInformation.__dict__,
+        payment_information = paymentInformation.__dict__,
+        order_information = orderInformation.__dict__,
+        point_of_sale_information = pointOfSaleInformation.__dict__
+    )
+
+
+    requestObj = del_none(requestObj.__dict__)
+    requestObj = json.dumps(requestObj)
+
+
+    try:
+        config_obj = configuration.Configuration()
+        client_config = config_obj.get_alternative_configuration()
+        api_instance = PaymentsApi(client_config)
+        return_data, status, body = api_instance.create_payment(requestObj)
+
+        print("\nAPI RESPONSE CODE : ", status)
+        print("\nAPI RESPONSE BODY : ", body)
+
+        write_log_audit(status)
+        return return_data
+    except Exception as e:
+        write_log_audit(e.status if hasattr(e, 'status') else 999)
+        print("\nException when calling PaymentsApi->create_payment: %s\n" % e)
+
+def write_log_audit(status):
+    print(f"[Sample Code Testing] [{Path(__file__).stem}] {status}")
+
+if __name__ == "__main__":
+    pin_debit_purchase_using_emv_technology_with_contactless_read_with_visa_platform_connect()
